be421e15befdf568535012847a542f75229e4514
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-11-24 22:28:36 +0100

    Update dashboard-routing.module.ts

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -5,10 +5,18 @@ import { DashboardPage } from './dashboard.page';
 import { CreateComponent } from './create/create.component';
 import { ReadComponent } from './read/read.component';
 
+import { AuthGuard } from '@auth0/auth0-angular';
+
 const routes: Routes = [
-  {path: '',component: DashboardPage},
+  {path: '',component: DashboardPage,
+
+  canActivate: [AuthGuard]
+
+
+},
   {path:'create',component:CreateComponent},
-  {path:'read',component:ReadComponent}
+  {path:'read',component:ReadComponent},
+
 ];
 
 @NgModule({
