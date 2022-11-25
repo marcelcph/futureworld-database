8da6076d364a74acccdf6a85e078897030d81990
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2022-11-25 01:51:29 +0100

    Update dashboard-routing.module.ts

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -4,19 +4,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardPage } from './dashboard.page';
 import { CreateComponent } from './create/create.component';
 import { ReadComponent } from './read/read.component';
-
 import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-  {path: '',component: DashboardPage,
+  {path: '',component: DashboardPage,    
+  pathMatch: 'full',
+  canActivate: [AuthGuard]},
 
-  canActivate: [AuthGuard]
+ 
 
-
-},
   {path:'create',component:CreateComponent},
   {path:'read/:id',component:CreateComponent},
-  {path:'read',component:ReadComponent}
+  {path:'read',component:ReadComponent},
 ];
 
 @NgModule({
